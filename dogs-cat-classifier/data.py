7c0f0c68afd0337b893b490ddf9f090c602465b4
Parth Rathod <17307743+psquare97@users.noreply.github.com>
2018-02-15 14:55:07 +0530

    Add files via upload

---
+++ dogs-cat-classifier/data.py
@@ -0,0 +1,79 @@
+import cv2
+import numpy as np
+import glob
+import sys
+import os
+import pickle
+
+# For doga [1,0]
+# For cats [0,1]
+def create_data():
+	image_size = 50
+	length = image_size*image_size
+	os.chdir('../dogscats/train/dogs')
+	pics_of_dogs = glob.glob('*.jpg')
+	os.chdir('../cats')
+	pics_of_cats = glob.glob('*.jpg')
+	os.chdir('..')
+	total_images = len(pics_of_cats) + len(pics_of_dogs)
+	
+	main_data = np.zeros([total_images,length+2],dtype='f')
+	i=0
+	os.chdir('dogs')
+
+	print("Total Dogs : ",len(pics_of_dogs),"Total Cats : ",len(pics_of_cats));
+	print("Reading images of dogs")
+	for pic in pics_of_dogs:
+		img = cv2.imread(pic,0)
+		img = cv2.resize(img,(image_size,image_size))
+		main_data[i,0:length] = np.reshape(img,(1,length))
+		main_data[i,length] = 1
+		i = i+1
+	print("Reading images of cats")
+	os.chdir('../cats')
+	for pic in pics_of_cats:
+		img = cv2.imread(pic,0)
+		img = cv2.resize(img,(image_size,image_size))
+		main_data[i,0:length] = np.reshape(img,(1,length))
+		main_data[i,length+1] = 1
+		i = i+1
+	os.chdir('../../../dogs-cat-classifier')
+	np.random.shuffle(main_data)
+	print("saving data set")
+	pickle.dump(main_data,open('main_data.p','wb'))
+	return 
+	
+def test_data():
+	image_size = 50
+	length = image_size*image_size
+	os.chdir('../dogscats/valid/dogs')
+	pics_of_dogs = glob.glob('*.jpg')
+	os.chdir('../cats')
+	pics_of_cats = glob.glob('*.jpg')
+	os.chdir('..')
+	total_images = len(pics_of_cats) + len(pics_of_dogs)
+	test_data = np.zeros([total_images,length+2],dtype='f')
+	i=0
+	os.chdir('dogs')
+	print("reading images of dogs")
+	for pic in pics_of_dogs:
+		img = cv2.imread(pic,0)
+		img = cv2.resize(img,(image_size,image_size))
+		test_data[i,0:length] = np.reshape(img,(1,length))
+		test_data[i,length] = 1
+		i = i+1
+	os.chdir('../cats')
+	print("Reading images of cats")
+	for pic in pics_of_cats:
+		img = cv2.imread(pic,0)
+		img = cv2.resize(img,(image_size,image_size))
+		test_data[i,0:length] = np.reshape(img,(1,length))
+		test_data[i,length+1] = 1
+		i = i+1
+	os.chdir('../../../dogs-cat-classifier')
+	np.random.shuffle(test_data)
+	print("Saving testing data set")
+	pickle.dump(test_data,open('test_data.p','wb'))	
+	return
+create_data()
+test_data()
